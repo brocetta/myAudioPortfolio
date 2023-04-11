4e8db1a4d386dbb3290e6a5146b255eda95c0722
brocetta <broceta@yahoo.com>
2023-04-11 16:05:35 +0200

    update img and font

--- YourAudioProducer.js
+++ YourAudioProducer.js
@@ -1,5 +1,4 @@
 const h1 = document.createElement("h1");
-const heroImg = document.createElement("img");
 const hero = document.createElement("div");
 
 
@@ -9,8 +8,6 @@ hero.classList.add("hero-section");
 h1.innerHTML = "Your Audio Producer";
 hero.append(h1);
 
-heroImg.src = "../media/DrumMachine.png";
-heroImg.alt = "Picture of Drum Machine";
 
 hero.append(heroImg);
 img.classList.add("hero-image");
