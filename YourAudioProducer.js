b98e050658eb5d0ee036279c9064ed5d7a46dd3c
brocetta <broceta@yahoo.com>
2023-12-25 11:47:11 +0100

    cleaning the code

--- YourAudioProducer.js
+++ YourAudioProducer.js
@@ -1,73 +1,32 @@
 import WaveSurfer from "https://unpkg.com/wavesurfer.js@7/dist/wavesurfer.esm.js";
 
-const pod1 = WaveSurfer.create({
-  container: "#pod1",
-  waveColor: "gray",
-  progressColor: "#df313c",
-  normalize: true,
-  url: "media/Audio/HCH 15.mp3",
-});
+function audioPlayer(nameString, link) {
+  const audio = WaveSurfer.create({
+    container: nameString,
+    waveColor: "gray",
+    progressColor: "#df313c",
+    normalize: true,
+    url: link,
+  });
 
-pod1.on("interaction", () => {
-  if (pod1.isPlaying()) {
-    pod1.pause();
-  } else {
-    pod1.play();
-  }
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.code === "Space") {
-    if (pod1.isPlaying()) {
-      pod1.pause();
+  audio.on("interaction", () => {
+    if (audio.isPlaying()) {
+      audio.pause();
+    } else {
+      audio.play();
     }
-  }
-});
+  });
 
-const pod2 = WaveSurfer.create({
-  container: "#pod2",
-  waveColor: "gray",
-  progressColor: "#df313c",
-  normalize: true,
-  url: "media/Audio/voice.mp3",
-});
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+      if (audio.isPlaying()) {
+        audio.pause();
+      }
+    }
+  });
+}
 
-pod2.on("interaction", () => {
-  if (pod2.isPlaying()) {
-    pod2.pause();
-  } else {
-    pod2.play();
-  }
-});
-
-const pod3 = WaveSurfer.create({
-  container: "#pod3",
-  waveColor: "gray",
-  progressColor: "#df313c",
-  normalize: true,
-  url: "media/Audio/HCH 15.mp3",
-});
-
-pod3.on("interaction", () => {
-  if (pod3.isPlaying()) {
-    pod3.pause();
-  } else {
-    pod3.play();
-  }
-});
-
-const pod4 = WaveSurfer.create({
-  container: "#pod4",
-  waveColor: "gray",
-  progressColor: "#df313c",
-  normalize: true,
-  url: "media/Audio/voice.mp3",
-});
-
-pod4.on("interaction", () => {
-  if (pod4.isPlaying()) {
-    pod4.pause();
-  } else {
-    pod4.play();
-  }
-});
+audioPlayer("#pod1", "media/Audio/HCH 15.mp3");
+audioPlayer("#pod2", "media/Audio/voice.mp3");
+audioPlayer("#pod3", "media/Audio/HCH 15.mp3");
+audioPlayer("#pod4", "media/Audio/voice.mp3");
