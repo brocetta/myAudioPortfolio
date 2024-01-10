40107d19bcf7ef2b658edd5ae36c69e494e8a4d0
brocetta <broceta@yahoo.com>
2024-01-10 13:09:03 +0100

    adding new section, adding small size video files, re-arranging sections and elements

--- YourAudioProducer.js
+++ YourAudioProducer.js
@@ -35,3 +35,37 @@ audioPlayer("#audiobook1", "media/audio/voice.mp3");
 audioPlayer("#audiobook2", "media/audio/HCH 15.mp3");
 audioPlayer("#audiobook3", "media/audio/voice.mp3");
 audioPlayer("#audiobook4", "media/audio/HCH 15.mp3");
+
+// REVIEWS
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+window.onload= function () {
+ setInterval(function(){ 
+     plusSlides(1);
+ }, 3000);
+ }
